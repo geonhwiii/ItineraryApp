633fc4f580cb9eab60c700c0f7928f0f7b7c9da2
geonhwiii <g01063962671@gmail.com>
2019-03-26 12:45:03 +0900

    Added readTrip code

---
+++ Itineryary App/Model/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  Itineryary App
+//
+//  Created by 정건휘 on 26/03/2019.
+//  Copyright © 2019 gunw. All rights reserved.
+//
+
+import Foundation
+
+class Data {
+   static var tripModels = [TripModel]()
+   
+}
